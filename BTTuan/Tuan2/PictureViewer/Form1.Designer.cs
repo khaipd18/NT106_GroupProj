4a34c1d4b72b1b39692b691dbcfd81c9f75099e9
Phan Dinh Khai <khaipd180405@gmail.com>
2024-09-19 19:56:53 +0700

    khaipd18 - Add functionality and user interface to the PV application - Week2

--- BTTuan/Tuan2/PictureViewer/Form1.Designer.cs
+++ BTTuan/Tuan2/PictureViewer/Form1.Designer.cs
@@ -2,15 +2,11 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelThumbnails;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +16,49 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.flowLayoutPanelThumbnails = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.SuspendLayout();
 
-        #endregion
+            this.pictureBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMain.TabIndex = 0;
+            this.pictureBoxMain.TabStop = false;
+
+            this.btnOpenFolder.Location = new System.Drawing.Point(12, 420);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(100, 23);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Open Folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+
+            this.flowLayoutPanelThumbnails.Location = new System.Drawing.Point(420, 12);
+            this.flowLayoutPanelThumbnails.Name = "flowLayoutPanelThumbnails";
+            this.flowLayoutPanelThumbnails.Size = new System.Drawing.Size(200, 400);
+            this.flowLayoutPanelThumbnails.TabIndex = 2;
+            this.flowLayoutPanelThumbnails.AutoScroll = true;
+            this.flowLayoutPanelThumbnails.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanelThumbnails.WrapContents = false;
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 450);
+            this.Controls.Add(this.flowLayoutPanelThumbnails);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.pictureBoxMain);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+        }
     }
 }
--- BTTuan/Tuan2/PictureViewer/Form1.cs
+++ BTTuan/Tuan2/PictureViewer/Form1.cs
@@ -1,10 +1,100 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace PictureViewer
 {
     public partial class Form1 : Form
     {
+        private string[] imageFiles; 
+        private int currentIndex = 0; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // nút Open Folder 
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = folderBrowserDialog.SelectedPath;
+                    LoadImagesFromFolder(folderPath);
+                }
+            }
+        }
+
+        // load ảnh từ thư mục
+        private void LoadImagesFromFolder(string folderPath)
+        {
+            imageFiles = Directory.GetFiles(folderPath, "*.*")
+                                  .Where(f => f.EndsWith(".jpg") || f.EndsWith(".jpeg") || f.EndsWith(".png"))
+                                  .ToArray();
+
+            if (imageFiles.Length > 0)
+            {
+                DisplayThumbnails(); // Hiển thị danh sách các hình thu nhỏ
+                currentIndex = 0;
+                DisplayImage(currentIndex); // Hiển thị ảnh đầu tiên trong PictureBox
+            }
+        }
+
+        // Hiển thị hình ảnh dựa trên chỉ số
+        private void DisplayImage(int index)
+        {
+            if (imageFiles != null && imageFiles.Length > 0 && index >= 0 && index < imageFiles.Length)
+            {
+                pictureBoxMain.Image = Image.FromFile(imageFiles[index]);
+            }
+        }
+
+        // Hiển thị các hình thu nhỏ
+        private void DisplayThumbnails()
+        {
+            flowLayoutPanelThumbnails.Controls.Clear(); 
+
+            foreach (string filePath in imageFiles)
+            {
+                PictureBox thumbnail = new PictureBox();
+                thumbnail.Image = Image.FromFile(filePath);
+                thumbnail.SizeMode = PictureBoxSizeMode.Zoom;
+                thumbnail.Size = new Size(100, 100); 
+                thumbnail.Click += (sender, e) =>
+                {
+                    currentIndex = Array.IndexOf(imageFiles, filePath);
+                    DisplayImage(currentIndex); 
+                };
+                flowLayoutPanelThumbnails.Controls.Add(thumbnail);
+            }
+        }
+
+        // phím mũi tên trái phải 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (imageFiles != null && imageFiles.Length > 0)
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    if (currentIndex < imageFiles.Length - 1)
+                    {
+                        currentIndex++;
+                        DisplayImage(currentIndex);
+                    }
+                }
+                else if (e.KeyCode == Keys.Left)
+                {
+                    if (currentIndex > 0)
+                    {
+                        currentIndex--;
+                        DisplayImage(currentIndex);
+                    }
+                }
+            }
         }
     }
 }
