f14c415e6d34201d19c55337129235318a906d15
Le Chanh An <161144795+LeChanhAn@users.noreply.github.com>
2024-09-24 13:45:56 +0700

    aan-updated ui

--- BTTuan/Tuan3/Windows Explorer/Form1.Designer.cs
+++ BTTuan/Tuan3/Windows Explorer/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(9, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 29);
+            button1.TabIndex = 0;
+            button1.Text = "<<";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(65, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 29);
+            button2.TabIndex = 1;
+            button2.Text = ">>";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(694, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Open...";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(119, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Path :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(162, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(526, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(11, 55);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(781, 343);
+            listView1.TabIndex = 5;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listView1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ListView listView1;
     }
 }
--- BTTuan/Tuan3/Windows Explorer/Form1.cs
+++ BTTuan/Tuan3/Windows Explorer/Form1.cs
@@ -1,10 +1,41 @@
-namespace Windows_Explorer
+﻿namespace Windows_Explorer
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            listView1.View = View.Details;
+
+            // Thêm các cột "Name", "Type", "Size", "Date modified"
+            listView1.Columns.Add("Name", 200); // 200 là chiều rộng của cột
+            listView1.Columns.Add("Type", 100);
+            listView1.Columns.Add("Size", 100);
+            listView1.Columns.Add("Date modified", 150);
+
+            // Tùy chọn: Cho phép người dùng thay đổi kích thước các cột
+            listView1.AllowColumnReorder = true;
+            listView1.FullRowSelect = true; // Chọn toàn bộ dòng
+            listView1.GridLines = true; // Hiển thị lưới
+        }
+        private void LoadFiles(string folderPath)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                // Tạo một ListViewItem cho mỗi file
+                ListViewItem item = new ListViewItem(file.Name); // Tên file
+                item.SubItems.Add(file.Extension); // Loại file (Type)
+                item.SubItems.Add(file.Length.ToString()); // Kích thước file (Size)
+                item.SubItems.Add(file.LastWriteTime.ToString()); // Ngày chỉnh sửa (Date modified)
+
+                // Thêm item vào ListView
+                listView1.Items.Add(item);
+            }
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
